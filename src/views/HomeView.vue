88ee1e6b9df5b8a3d8003679c57bdaca1888af06
Abraão Oliveira <113057132+abraao0liveira@users.noreply.github.com>
2023-06-10 03:32:49 -0300

    update styles HomeContainer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,23 @@
 </script>
 
 <template>
-  <h1>Home</h1>
+  <div className="HomeContainer">
+    <h1>Home</h1>
+  </div>
 </template>
 
 <style>
+  .HomeContainer {
+    max-width: 74rem;
+    min-height: calc(100vh - 10rem);
+    margin: 3rem auto;
+    padding: 2.5rem;
+
+    background: var(--gray-800);
+    border-radius: 8px;
+
+    display: flex;
+    flex-direction: column;
+  }
 </style>
 
