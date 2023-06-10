6bb199ace760a45af4813b14deea3b8891ea1b4d
Abraão Oliveira <113057132+abraao0liveira@users.noreply.github.com>
2023-06-10 05:24:16 -0300

    create cut url aplication

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,52 @@
 <script setup>
+  function Cut() {
+    const url = document.getElementById("inputUrl").value
+    if (!url) {
+      alert("Insira uma URL valida.")
+      return
+    }
+    //apiKey: 7af07ee0971342da866ae0935a17b727
+    const headers = {
+      "Content-Type": "application/json",
+      "apiKey": "7af07ee0971342da866ae0935a17b727"
+    }
+    const linkRequest = {
+      destination: url,
+      domain: { fullName: "rebrand.ly" }
+    }
+    fetch("https://api.rebrandly.com/v1/links", {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(linkRequest)
+    })
+    .then((response)  => response.json())
+    .then((json) => {
+      let inputUrl = document.getElementById("inputUrl")
+      inputUrl.value = json.shortUrl
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+
+  function Copy() {
+    const inputUrl = document.getElementById("inputUrl")
+    inputUrl.select()
+    inputUrl.setSelectionRange(0, 99999)
+    
+    navigator.clipboard.writeText(inputUrl.value)
+    alert("Copiado para a área de transferência.")
+  }
 </script>
 
 <template>
   <div className="HomeContainer">
-    <h1>Home</h1>
+    <h1>Reduza sua URL e compartilhe mais com menos!</h1>
+    <div className="boxInput">
+      <input className="url" id="inputUrl" type="text" placeholder="Coloque sua URL">
+      <button v-on:click="Cut">Encurtar</button>
+      <button v-on:click="Copy">Copiar</button>
+    </div>
   </div>
 </template>
 
@@ -12,13 +55,50 @@
     max-width: 74rem;
     min-height: calc(100vh - 10rem);
     margin: 3rem auto;
-    padding: 2.5rem;
 
     background: var(--gray-800);
     border-radius: 8px;
 
     display: flex;
+    align-items: center;
+    justify-content: center;
     flex-direction: column;
   }
+  h1 {
+    font-size: 30px;
+    margin-bottom: 30px;
+  }
+  .boxInput {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin-bottom: 35px;
+  }
+  .url {
+    padding: 12px 20px;
+    text-align: center;
+    outline: transparent;
+    width: 40%;
+    border-radius: 25px;
+    font-size: 20px;
+    color: var(--gray-800);
+    border: none
+  }
+  button {
+    padding: 15px 20px;
+    width: 160px;
+    margin-left: 10px;
+    border-radius: 25px;
+    background-color: var(--green-500);
+    color: var(--white);
+    font-size: 20px;
+    border: none;
+    cursor: pointer;
+  }
+  button:hover {
+    background-color: var(--green-700);
+    transition: background-color 0.2s;
+  }
+
 </style>
 
